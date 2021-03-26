c24722a2a826079d68dfa3dc0665c3ef71c4cb68
Nora Schinkel <ncschinkel@gmail.com>
2021-03-26 16:10:48 +0100

    Add an API to retrieve all available gqueries

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v3 do
       resources :areas, :only => [:index, :show]
+      resources :gqueries, :only => :index
       resources :scenarios, :only => [:show, :create, :update] do
         member do
           get :batch
